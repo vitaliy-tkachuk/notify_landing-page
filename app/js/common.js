96578371741926ff21369b85ef1db34ac8ad0f81
VitFL <vitfl@protonmail.com>
2019-01-25 13:10:55 +0200

    landing page; gulp-gh-pages

--- app/js/common.js
+++ app/js/common.js
@@ -2,4 +2,33 @@ $(function() {
 
 	// Custom JS
 
+	$(document).ready(function(){
+		$(".owl-carousel").owlCarousel({
+			center: true,
+   	 	items: 1,
+			loop: true,
+			dots: true,
+			dotsContainer: '#carousel-custom-dots',
+			startPosition: randomPosition()
+		});
+		//$(this).index()
+		$('.owl-dot').click(function(){$(".owl-carousel").trigger('to.owl.carousel', [$(this).index(), 300]);});
+		//Random index generator
+		function randomPosition(){
+		var r_hb = $('#carousel-custom-dots li').length;
+		var randomize = null;
+		
+		
+		randomize=1
+		
+		
+		if (randomize != 0) {
+		return (Math.floor(Math.random()* r_hb));
+		}
+		else { return 0 ;}
+		}
+
+	});
+
 });
+
